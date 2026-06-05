7a3b96bcf455abe55386983e58f4efbdbd908415
Niraj Shah <nirajshah950@gmail.com>
2026-06-05 22:50:27 +0530

    Implementation - Define the model to store the User Details.
    1. set the fullName, email, password, phone as nullable.
    2. use true as the default value for isActive.

--- src/main/java/com/expensive/Expensive/Tracker/entity/User.java
+++ src/main/java/com/expensive/Expensive/Tracker/entity/User.java
@@ -1,4 +1,35 @@
 package com.expensive.Expensive.Tracker.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Data
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private long phone;
+
+    @ColumnDefault("1")
+    private boolean isActive;
 }
