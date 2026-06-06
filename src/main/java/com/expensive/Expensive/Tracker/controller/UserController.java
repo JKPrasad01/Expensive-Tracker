3ec283977e93679f06261716aa74174acfc96798
Niraj Shah <nirajshah950@gmail.com>
2026-06-07 00:00:52 +0530

    UPDATE - Fixed the bugs mentioned by the Prasad.

--- src/main/java/com/expensive/Expensive/Tracker/controller/UserController.java
+++ src/main/java/com/expensive/Expensive/Tracker/controller/UserController.java
@@ -6,6 +6,8 @@ import com.expensive.Expensive.Tracker.service.UserService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,8 +21,11 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    ResponseDTO signup(@Valid @RequestBody UserDTO userDTO){
-        return userService.signup(userDTO);
+    public ResponseEntity<ResponseDTO> signup(@Valid @RequestBody UserDTO userDTO){
+        ResponseDTO resp = userService.signup(userDTO);
+//        return new ResponseEntity<>(resp,resp.getStatusCode());
+//        return ResponseEntity.status(resp.getStatusCode().value()).body(resp);
+        return ResponseEntity.ok(resp);
     }
 
 }
