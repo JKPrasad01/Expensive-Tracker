7f079fc6d688d5431a23bdb700cbdb18d9a18aa5
Niraj Shah <nirajshah950@gmail.com>
2026-06-06 17:17:15 +0530

    NEW - Create an class for the Signup User.
    1. accepts the User Entity variables to create the User.

--- src/main/java/com/expensive/Expensive/Tracker/dto/UserDTO.java
+++ src/main/java/com/expensive/Expensive/Tracker/dto/UserDTO.java
@@ -1,4 +1,18 @@
 package com.expensive.Expensive.Tracker.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDTO {
+    private String fullName;
+    private String email;
+    private String password;
+    private String phone;
+    private long role;
 }
