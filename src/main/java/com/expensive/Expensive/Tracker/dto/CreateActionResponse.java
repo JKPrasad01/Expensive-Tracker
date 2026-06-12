a31dcc07c0450a8c48c0f3fdc8acc9a43651c56a
Niroj Shah <105719758+NirojShah@users.noreply.github.com>
2026-06-12 08:28:30 +0530

    Revert "Feature - Action Entiry"

--- src/main/java/com/expensive/Expensive/Tracker/dto/CreateActionResponse.java
+++
@@ -1,9 +0,0 @@
-import lombok.Data;
-
-@Data
-public class CreateActionResponse{
-    private Integer id;
-    private String actionName;
-    private String actionKey;
-    private String description;
-}
